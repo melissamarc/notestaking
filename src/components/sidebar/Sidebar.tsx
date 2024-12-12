ae43f682b6ae47eda055566191adae772c86c774
Wilson Leonardo <melmarc1moreira@gmail.com>
2024-12-11 21:25:14 -0300

    adicionei alguns botoes e mudei o design

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -4,7 +4,8 @@ export default function Sidebar() {
 
     return (
         <div className='sidebar'>
-            <img src='https://i.pinimg.com/736x/d0/b0/9b/d0b09bf3367886bb48ce0aaaee8e3bad.jpg' className='logo' />
+            <button> nova nota </button>
+            <button> todas </button>
             <button> arquivadas </button>
 
         </div>
